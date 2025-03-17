e558ea0983f279f5779dcca021c402d3c76db4fe
kaany <74436329+Kaanydev@users.noreply.github.com>
2025-03-17 19:51:05 -0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -56,17 +56,17 @@ if continuacao0 == "Sim":
     time.sleep(1)
     os.system("cls")
     print("\033[35mPergunta 2\033[m")
-    print("\033[35mQual foi a motivação ta 1° guerra mundial?\033[m")
-    A = print("Opção A: O assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austríaco, e sua esposa no dia 28 de junho de 1914.\033")
-    B = print("Opção B: Foi uma consequência da grande expansão de crédito por meio de oferta monetária.\033")
-    C = print("Opção C: A divisão do país em duas zonas com governos ideologicamente distintos em 1945 e a invasão do sul promovida pelas tropas do norte em junho de 1950.\033")
-    D = print("Opção D: A primeira grande guerra, a economia, o descontentamento e a desorganização do aparato estatal russo.\033")
+    print("\033[35mA escola de Paraqui organizou uma Olimpíada de Matemática para seus 250 alunos e premiou com medalhas os 8% que obtiveram as notas mais altas. Quantas medalhas foram distribuídas?\033[m")
+    A = print("Opção A: 8\033")
+    B = print("Opção B: 11\033")
+    C = print("Opção C: 14\033")
+    D = print("Opção D: 20\033")
 
     while True:
         print("Lembre-se de enviar sua resposta em letra MAIUSCULA")
         pergunta2 = str(input("Qual das alternativas acima está correta?"))
-        rpcerta2 = A
-        if pergunta2 == "A":
+        rpcerta2 = C
+        if pergunta2 == "C":
             pontuacao+=1
             resposta2 = (f"\033[36;32mA resposta está certa, você ganhou 1 ponto, você possui {pontuacao} Pontos\033[m")
             print(resposta2)
@@ -96,11 +96,11 @@ if continuacao1 == "Sim":
     time.sleep(1)
     os.system("cls")
     print("\033[35mPergunta 3\033[m")
-    print("\033[35mQual foi a motivação ta 1° guerra mundial?\033[m")
-    A = print("Opção A: O assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austríaco, e sua esposa no dia 28 de junho de 1914.\033")
-    B = print("Opção B: Foi uma consequência da grande expansão de crédito por meio de oferta monetária.\033")
-    C = print("Opção C: A divisão do país em duas zonas com governos ideologicamente distintos em 1945 e a invasão do sul promovida pelas tropas do norte em junho de 1950.\033")
-    D = print("Opção D: A primeira grande guerra, a economia, o descontentamento e a desorganização do aparato estatal russo.\033")
+    print("\033[35mAs redes de alta tensão para transmissão de energia elétrica geram campo magnético variável o suficiente para induzir corrente elétrica no arame das cercas. Tanto os animais quanto os funcionários das propriedades rurais ou das concessionárias de energia devem ter muito cuidado ao se aproximarem de uma cerca quando esta estiver próxima a uma rede de alta tensão, pois, se tocarem no arame da cerca, poderão sofrer choque elétrico.\033[m")
+    A = print("Opção A:  Fazer o aterramento dos arames da cerca.\033")
+    B = print("Opção B: Acrescentar fusível de segurança na cerca.\033")
+    C = print("Opção C: Realizar o aterramento da rede de alta tensão.\033")
+    D = print("Opção D: Instalar fusível de segurança na rede de alta tensão.\033")
     while True:
         print("Lembre-se de enviar sua resposta em letra MAIUSCULA")
         pergunta3 = str(input("Qual das alternativas acima está correta?"))
